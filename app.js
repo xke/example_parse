c2f464bfa27e862b7b80f1a25af7c3dd77c3016d
Boris <bmisnik@ua-kv-w-26.cee.grey.global>
2013-08-21 12:37:51 +0300

    change port

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 8081);
+app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-;
+
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
